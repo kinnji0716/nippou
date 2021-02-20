9dbce253897571a7208edb7c62772d653e898c2d
kinnji.tooyama <kinnchann0716@mail.com>
2021-02-21 03:52:59 +0900

    Add Employee new

--- src/controllers/employees/EmployeesIndexServlet.java
+++ src/controllers/employees/EmployeesIndexServlet.java
@@ -18,13 +18,13 @@ import utils.DBUtil;
  * Servlet implementation class IndexServlet
  */
 @WebServlet("/employees/index")
-public class IndexServlet extends HttpServlet {
+public class EmployeesIndexServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public IndexServlet() {
+    public EmployeesIndexServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
