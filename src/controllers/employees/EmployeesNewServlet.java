f522d10e476a729d1a7ab026760ed7e39642238a
kinnji.tooyama <kinnchann0716@mail.com>
2021-02-21 04:25:05 +0900

    Add Employee create

--- src/controllers/employees/EmployeesNewServlet.java
+++ src/controllers/employees/EmployeesNewServlet.java
@@ -34,7 +34,7 @@ public class EmployeesNewServlet extends HttpServlet {
         request.setAttribute("_token", request.getSession().getId());
         request.setAttribute("employee", new Employee());
 
-        RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/views/employees/new.jsp");
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/new.jsp");
         rd.forward(request, response);
     }
 
